c17d6ca79a4b7e43ab1c4f13e851e39db1b35cce
Pirmin Költzsch <pirmin.koeltzsch@isecng.de>
2026-03-23 12:22:40 +0100

    update to 4.14.4

--- pkgs/wazuh-agent/default.nix
+++ pkgs/wazuh-agent/default.nix
@@ -1,116 +1,122 @@
 # pkgs/wazuh-agent/default.nix
-{ lib
-, stdenv
-, fetchFromGitHub
-, fetchurl
-, cmake
-, gnumake
-, gcc
-, python3
-, perl
-, curl
-, gnutar
-, gzip
-, bzip2
-, xz
-, automake
-, autoconf
-, libtool
-, makeWrapper
-, openssl
-, zlib
-, pkg-config
-, expat
-, which
-, git
-, attr
-, clang
-, elfutils
-, patchelf
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  fetchurl,
+  cmake,
+  gnumake,
+  gcc,
+  python3,
+  perl,
+  curl,
+  gnutar,
+  gzip,
+  bzip2,
+  xz,
+  automake,
+  autoconf,
+  libtool,
+  makeWrapper,
+  openssl,
+  zlib,
+  pkg-config,
+  expat,
+  which,
+  git,
+  attr,
+  clang,
+  elfutils,
+  patchelf,
 }:
 
 let
-  version   = "4.14.3";
-  depsVer   = "49";
-  depsBase  = "https://packages.wazuh.com/deps/${depsVer}/libraries/sources";
+  version = "4.14.4";
+  depsVer = "99-29585";
+  depsBase = "https://packages.wazuh.com/deps/${depsVer}/libraries/sources";
 
-  dep = name: sha256: fetchurl {
-    url    = "${depsBase}/${name}.tar.gz";
-    inherit sha256;
-  };
+  dep =
+    name: sha256:
+    fetchurl {
+      url = "${depsBase}/${name}.tar.gz";
+      inherit sha256;
+    };
 
   nlohmannBase = "https://packages.wazuh.com/deps/21/libraries/sources";
-  depNlohmann  = sha256: fetchurl {
-    url = "${nlohmannBase}/nlohmann.tar.gz";
-    inherit sha256;
-  };
+  depNlohmann =
+    sha256:
+    fetchurl {
+      url = "${nlohmannBase}/nlohmann.tar.gz";
+      inherit sha256;
+    };
 
   modernBpfC = fetchurl {
-    url    = "https://raw.githubusercontent.com/wazuh/wazuh/v4.12.0/src/syscheckd/src/ebpf/src/modern.bpf.c";
+    url = "https://raw.githubusercontent.com/wazuh/wazuh/v4.12.0/src/syscheckd/src/ebpf/src/modern.bpf.c";
     sha256 = "sha256-mnPGgEoBZgXT6KxNCHEYt8eZHqrWIJjajxwiifpff+A=";
   };
 
   libbpfSrc = fetchFromGitHub {
-    owner  = "libbpf";
-    repo   = "libbpf";
-    rev    = "v1.5.0";
+    owner = "libbpf";
+    repo = "libbpf";
+    rev = "v1.5.0";
     sha256 = "sha256-+L/rbp0a3p4PHq1yTJmuMcNj0gT5sqAPeaNRo3Sh6U8=";
   };
 
   bpftoolSrc = fetchFromGitHub {
-    owner  = "libbpf";
-    repo   = "bpftool";
-    rev    = "v7.5.0";
+    owner = "libbpf";
+    repo = "bpftool";
+    rev = "v7.5.0";
     sha256 = "sha256-XUh53pZ4NS56aMZMxYFVxrS+yScH0M7NiRGwjw8P3NE=";
     fetchSubmodules = true;
   };
 
   vmlinuxSrc = fetchFromGitHub {
-    owner  = "libbpf";
-    repo   = "vmlinux.h";
-    rev    = "main";
+    owner = "libbpf";
+    repo = "vmlinux.h";
+    rev = "main";
     sha256 = "sha256-EKLJh3sSH/BGMZHUfivQ8J1QbtnybekghZ+LNvdj858=";
   };
 
   externalDeps = {
-    cJSON              = dep "cJSON"              "sha256-2oCfcLfQOsUprmIj1DkL+ibNKfjDLI6LO2Me+hZniS0=";
-    openssl            = dep "openssl"            "sha256-A4b+Ogv0i64spNF0KlPfmo/LG3NYO6Iuj4p936E3XNk=";
-    zlib               = dep "zlib"               "sha256-tZ04FJ8MKexU0nZmEevFpRoDK/lxfjmprwD7bLhTK4s=";
-    curl               = dep "curl"               "sha256-MM9xQuQoJxjOsjfhe1y/da/NfJ84gKA5xe/qYtsJRwk=";
-    bzip2              = dep "bzip2"              "sha256-J2iO4DFqZLOeURssIkBwytl8OUpfcR+dBV/BgJ2JW80=";
-    libpcre2           = dep "libpcre2"           "sha256-WoDWVNfRSz25+jpJ179EpJhoO0Z4SojOxRSosZR2e5I=";
-    libyaml            = dep "libyaml"            "sha256-NdqtYIs3LVzgmfc4wPIb/MA9aSDZL0SDhsWE5mTxN2o=";
-    msgpack            = dep "msgpack"            "sha256-BtY7zzKJbNCvVIDEARNLGtHBZv2E6+W0hueSEB7oVOI=";
-    sqlite             = dep "sqlite"             "sha256-qBv/MLtK/9GwakmD/4jvgntKuuoxkbOa/37bKNHd0AM=";
-    nlohmann           = depNlohmann              "sha256-tcOpnp61Mx2VjivdOmKDxLnqetZ03UZp7ibVxe74Rf4=";
-    libarchive         = dep "libarchive"         "sha256-VA/0pV3vp1d4osQFZ6gwZIzlNnuK6hIzZodNlrc074A=";
-    lua                = dep "lua"                "sha256-Iz6H6HEJC9MMS2kqxzvXFDYcFQURSOTu7IKKHfhDbso=";
-    popt               = dep "popt"               "sha256-1ogKBmIsoy3EqjmtXc977y+qgb2TGvvmS6Q0rY/uHao=";
-    libdb              = dep "libdb"              "sha256-fpxE6Mf9sYb/UhqNCFsb+mNNNC3Md37Oofv5qYq13F4=";
-    libffi             = dep "libffi"             "sha256-DpcfZLrMIglOifA0u6B1tA7MLCwpAO7NeuhYFf1sn2k=";
-    procps             = dep "procps"             "sha256-Ih85XinRvb5LrMnbOWAu7guuaFqTVDe+DX/rQuMZLQc=";
-    "audit-userspace"  = dep "audit-userspace"    "sha256-6Coy5e35OwVRYOFLyX9B3q05KHklhR3ICnY44tTTBDQ=";
-    googletest         = dep "googletest"         "sha256-jB6KCn8iHCEl6Z5qy3CdorpHJHa00FfFjeUEvr841Bc=";
-    libplist           = dep "libplist"           "sha256-iCeNS9/BvWo6GlWk89kzaD0nMroJz3p0n+jsjuxAbjw=";
-    pacman             = dep "pacman"             "sha256-Yxq+Bl7JgttWv+wFzzfDOo9on9f0Fkw5J3KEvXoNDjE=";
-    rpm                = dep "rpm"                "sha256-kNhy9U6rzzdzbZCsF6jTEwEtqE5oWxn9Lav0oBKVcpA=";
-    rocksdb            = dep "rocksdb"            "sha256-7u1go9Tin3MF55+fXOvUJhF0JhIn8bWn0F2lVWVnVDY=";
-    lzma               = dep "lzma"               "sha256-TODBktQQcrVnmvibtTHvtoXIJnpLfiAFmZFJrBcCgTQ=";
-    "cpp-httplib"      = dep "cpp-httplib"        "sha256-ZRdXMmNhFoa5IZunlsNfVKMG6yfcPHLhgH8qCjTKweg=";
-    benchmark          = dep "benchmark"          "sha256-lMV6oMsr142+nnfTMsvGRNrw/s3JoJYyBIvm4J+c7Ws=";
-    "libbpf-bootstrap" = dep "libbpf-bootstrap"   "sha256-hh74B1ePDobIfuXC2YdHaFPmQGm/xLsTon4jPtNXSDI=";
-    dbus               = dep "dbus"               "sha256-fGVKyaT2i1DzLWdJIsGP/nQdMoqX15RFdhj8OgekjTo=";
+    cJSON = dep "cJSON" "sha256-2oCfcLfQOsUprmIj1DkL+ibNKfjDLI6LO2Me+hZniS0=";
+    openssl = dep "openssl" "sha256-A4b+Ogv0i64spNF0KlPfmo/LG3NYO6Iuj4p936E3XNk=";
+    zlib = dep "zlib" "sha256-tZ04FJ8MKexU0nZmEevFpRoDK/lxfjmprwD7bLhTK4s=";
+    curl = dep "curl" "sha256-MM9xQuQoJxjOsjfhe1y/da/NfJ84gKA5xe/qYtsJRwk=";
+    bzip2 = dep "bzip2" "sha256-J2iO4DFqZLOeURssIkBwytl8OUpfcR+dBV/BgJ2JW80=";
+    libpcre2 = dep "libpcre2" "sha256-WoDWVNfRSz25+jpJ179EpJhoO0Z4SojOxRSosZR2e5I=";
+    libyaml = dep "libyaml" "sha256-NdqtYIs3LVzgmfc4wPIb/MA9aSDZL0SDhsWE5mTxN2o=";
+    msgpack = dep "msgpack" "sha256-BtY7zzKJbNCvVIDEARNLGtHBZv2E6+W0hueSEB7oVOI=";
+    sqlite = dep "sqlite" "sha256-qBv/MLtK/9GwakmD/4jvgntKuuoxkbOa/37bKNHd0AM=";
+    nlohmann = depNlohmann "sha256-tcOpnp61Mx2VjivdOmKDxLnqetZ03UZp7ibVxe74Rf4=";
+    libarchive = dep "libarchive" "sha256-VA/0pV3vp1d4osQFZ6gwZIzlNnuK6hIzZodNlrc074A=";
+    lua = dep "lua" "sha256-Iz6H6HEJC9MMS2kqxzvXFDYcFQURSOTu7IKKHfhDbso=";
+    popt = dep "popt" "sha256-1ogKBmIsoy3EqjmtXc977y+qgb2TGvvmS6Q0rY/uHao=";
+    libdb = dep "libdb" "sha256-fpxE6Mf9sYb/UhqNCFsb+mNNNC3Md37Oofv5qYq13F4=";
+    libffi = dep "libffi" "sha256-DpcfZLrMIglOifA0u6B1tA7MLCwpAO7NeuhYFf1sn2k=";
+    procps = dep "procps" "sha256-Ih85XinRvb5LrMnbOWAu7guuaFqTVDe+DX/rQuMZLQc=";
+    "audit-userspace" = dep "audit-userspace" "sha256-6Coy5e35OwVRYOFLyX9B3q05KHklhR3ICnY44tTTBDQ=";
+    googletest = dep "googletest" "sha256-jB6KCn8iHCEl6Z5qy3CdorpHJHa00FfFjeUEvr841Bc=";
+    libplist = dep "libplist" "sha256-iCeNS9/BvWo6GlWk89kzaD0nMroJz3p0n+jsjuxAbjw=";
+    pacman = dep "pacman" "sha256-Yxq+Bl7JgttWv+wFzzfDOo9on9f0Fkw5J3KEvXoNDjE=";
+    rpm = dep "rpm" "sha256-kNhy9U6rzzdzbZCsF6jTEwEtqE5oWxn9Lav0oBKVcpA=";
+    rocksdb = dep "rocksdb" "sha256-7u1go9Tin3MF55+fXOvUJhF0JhIn8bWn0F2lVWVnVDY=";
+    lzma = dep "lzma" "sha256-TODBktQQcrVnmvibtTHvtoXIJnpLfiAFmZFJrBcCgTQ=";
+    "cpp-httplib" = dep "cpp-httplib" "sha256-ZRdXMmNhFoa5IZunlsNfVKMG6yfcPHLhgH8qCjTKweg=";
+    benchmark = dep "benchmark" "sha256-lMV6oMsr142+nnfTMsvGRNrw/s3JoJYyBIvm4J+c7Ws=";
+    "libbpf-bootstrap" = dep "libbpf-bootstrap" "sha256-hh74B1ePDobIfuXC2YdHaFPmQGm/xLsTon4jPtNXSDI=";
+    dbus = dep "dbus" "sha256-fGVKyaT2i1DzLWdJIsGP/nQdMoqX15RFdhj8OgekjTo=";
   };
 
   src = fetchFromGitHub {
-    owner          = "wazuh";
-    repo           = "wazuh";
-    rev            = "v${version}";
-    sha256         = "sha256-mtQ8nXfliJSr7jyvAcD/cVbHw8t+1u6R15AYQoKkihY=";
+    owner = "wazuh";
+    repo = "wazuh";
+    rev = "v${version}";
+    sha256 = "sha256-mtQ8nXfliJSr7jyvAcD/cVbHw8t+1u6R15AYQoKkihY=";
     fetchSubmodules = true;
   };
 
-in stdenv.mkDerivation rec {
+in
+stdenv.mkDerivation rec {
   pname = "wazuh-agent";
   inherit version src;
 
@@ -121,31 +127,48 @@ in stdenv.mkDerivation rec {
   dontConfigure = true;
 
   nativeBuildInputs = [
-    cmake gnumake gcc python3 perl
-    curl gnutar gzip bzip2 xz
-    automake autoconf libtool
+    cmake
+    gnumake
+    gcc
+    python3
+    perl
+    curl
+    gnutar
+    gzip
+    bzip2
+    xz
+    automake
+    autoconf
+    libtool
     makeWrapper
     pkg-config
     expat
-    which git
-    clang clang.cc
+    which
+    git
+    clang
+    clang.cc
     elfutils
     patchelf
   ];
 
   buildInputs = [
-    openssl zlib
+    openssl
+    zlib
     attr
     stdenv.cc.cc.lib
   ];
 
   postUnpack = ''
     mkdir -p source/src/external
-    ${lib.concatStringsSep "\n" (lib.mapAttrsToList (dirName: drv: ''
-      echo "Extracting ${dirName}..."
-      mkdir -p source/src/external/${dirName}
-      tar -xf ${drv} -C source/src/external/${dirName} --strip-components=${if dirName == "sqlite" then "2" else "1"}
-      '') externalDeps)}
+    ${lib.concatStringsSep "\n" (
+      lib.mapAttrsToList (dirName: drv: ''
+        echo "Extracting ${dirName}..."
+        mkdir -p source/src/external/${dirName}
+        tar -xf ${drv} -C source/src/external/${dirName} --strip-components=${
+          if dirName == "sqlite" then "2" else "1"
+        }
+      '') externalDeps
+    )}
     substituteInPlace source/src/external/openssl/config \
       --replace 'exec "$THERE/Configure"' 'exec "$(cd "$THERE" && pwd)/Configure"'
     patchShebangs source/src/external/openssl/Configure
@@ -309,10 +332,10 @@ in stdenv.mkDerivation rec {
     chmod +x $INST/bin/* 2>/dev/null || true
     makeWrapper $INST/bin/wazuh-agentd $out/bin/wazuh-agentd \
       --set WAZUH_HOME /var/lib/wazuh-agent
-    if [ -x $INST/bin/wazuh-control ]; then
-      makeWrapper $INST/bin/wazuh-control $out/bin/wazuh-control \
-        --set WAZUH_HOME /var/lib/wazuh-agent
-    fi
+
+    makeWrapper $INST/bin/wazuh-control $out/bin/wazuh-control \
+      --set WAZUH_HOME /var/lib/wazuh-agent
+
     makeWrapper $INST/bin/agent-auth $out/bin/agent-auth \
       --set WAZUH_HOME /var/lib/wazuh-agent
 
@@ -320,10 +343,10 @@ in stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    description  = "Wazuh open-source security agent (XDR/SIEM endpoint component)";
-    homepage     = "https://wazuh.com";
-    license      = licenses.gpl2Only;
-    platforms    = [ "x86_64-linux" ];
-    maintainers  = [];
+    description = "Wazuh open-source security agent (XDR/SIEM endpoint component)";
+    homepage = "https://wazuh.com";
+    license = licenses.gpl2Only;
+    platforms = [ "x86_64-linux" ];
+    maintainers = [ ];
   };
 }
